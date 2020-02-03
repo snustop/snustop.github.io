66fe305738c244043465a422a67acd2df77c36a9
snustop <snustop.com.ua@gmail.com>
2020-02-03 22:26:10 +0200

    Updated Telegram

--- telegram.php
+++ telegram.php
@@ -39,7 +39,7 @@ for($i = 0; $i < count($order_list); $i++) {
 
 $txt = $order_message.$txt;
 
-// $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+$sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
 
 $txt = str_replace("%0A","<br/>",$txt);
 $to = "snustop.com.ua@gmail.com";
