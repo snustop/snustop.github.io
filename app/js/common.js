ffa983bbc00882d033a2f9ab023444f2545808ef
snustop <33807492+snustop@users.noreply.github.com>
2020-05-31 12:17:19 +0300

    update app folder

--- app/js/common.js
+++ app/js/common.js
@@ -6,8 +6,23 @@
 			$('#filters-wrap').stop().slideToggle(300);
 			$('#filter-btn-slide i').toggleClass('rotate-arrow');
 		});
+
+		$('#filters-wrap a').on('click', function(){
+			$('#filters-wrap').hide();
+			$('#filter-btn-slide i').toggleClass('rotate-arrow');
+		});
 	}
 	// filter slide (992) end
+
+
+	// Filter раздаем класс active START
+	// из-за PJAX потому что страница не перезагружается
+	var slideItemLi = $('#filters-wrap li');
+	$(slideItemLi).on('click', function(){
+		$(slideItemLi).removeClass('active');
+		$(this).addClass('active');
+	});
+	// Filter раздаем класс active END
 
 	// arrow to top START
 	var root = $('html, body');
@@ -39,7 +54,8 @@
 		// Трясти корзину при добавлении товара (при нажатии на кнопку добавить в корзину) END
 
 		// DISCOUNT START
-		$('#discount-btn').on('click', function() { /* при клике на кнопку ПРИМЕНИТЬ КУПОН вызывается ф-ция */
+		// при уводе фокуса и изменении применять промокод, т.к. иногда забывают нажимать на кнопку
+		$('#promo-code').on('change paste keyup', function() {
 			addDiscount();
 		});
 
@@ -51,15 +67,15 @@
 			var promCode = $('#promo-code').val().toLowerCase(); /* значения поля КУПОНА */
 			var cartRate = $('#cart-rate'); /* описание скидки % и грн */
 
-			if (promCode == "5disc") {
+			if (totalItem >= 5 && totalItem < 10) {
 				cartDiscount.text(Math.ceil(totalPrice * .95) + " грн"); /* скидка 5% */
 				var totalDiscount = totalPrice - Math.ceil(totalPrice * .95);
 				cartRate.text('Скидка 5%: ' + totalDiscount + ' грн');
 
-			}	else if (totalItem >= 10 && promCode == "10cans") { /* если в корзине 10 или больше товаров и промокод = 10cans то ... */
-				cartDiscount.text(Math.ceil(totalPrice * .93) + " грн"); /* скидка 7% */
-				var totalDiscount = totalPrice - Math.ceil(totalPrice * .93);
-				cartRate.text('Скидка 7%: ' + totalDiscount + ' грн');
+			}	else if (totalItem >= 10) { /* если в корзине 10 или больше товаров и промокод то ... */
+				cartDiscount.text(Math.ceil(totalPrice * .9) + " грн"); /* скидка 7% */
+				var totalDiscount = totalPrice - Math.ceil(totalPrice * .9);
+				cartRate.text('Скидка 10%: ' + totalDiscount + ' грн');
 
 			} 
 
@@ -68,9 +84,9 @@
 			}
 
 			if (promCode == "burda" || promCode == "zak" || promCode == "koval" || promCode == "kolosov" || promCode=="kochergin" || promCode=="nechaev" || promCode=="past" || promCode=="dvorak" || promCode=="black" || promCode=="ira" || promCode=="днепр" || promCode=="заря" || promCode=="шахта" || promCode=="динамо" || promCode=="zah") {
-				cartDiscount.text(Math.ceil(totalPrice * .9) + " грн"); /* скидка 10% */
-				var totalDiscount = totalPrice - Math.ceil(totalPrice * .9);
-				cartRate.text('Скидка 10%: ' + totalDiscount + ' грн');
+				cartDiscount.text(Math.ceil(totalPrice * .88) + " грн"); /* скидка 10% */
+				var totalDiscount = totalPrice - Math.ceil(totalPrice * .88);
+				cartRate.text('Скидка 12%: ' + totalDiscount + ' грн');
 			}
 
 		};
@@ -94,7 +110,7 @@
 				return item.el.attr('title') + '<small>by Telegram <a href="//t.me/snustopUA" target="_blank" title="telegram"><i class="icon-telegram"></i>snustopUA</a></small>';
 			}
 		}
-		});
+	});
 		// MAGNIFIC POPUP на главной section instagram END
 
 
@@ -155,11 +171,20 @@
 			localStorage.setItem('coupon', promCodeForm);
 			// запишем в local storage END
 
+			var allFields = formMail.serialize();
+			var orderList = JSON.parse(localStorage.getItem('simpleCart_items'));
+			var orderListArray =[];
+			for(var i in orderList) {
+				orderListArray.push(orderList[i]);
+			}
+
 			$.ajax({
-				url: "//formspree.io/snustop.com.ua@gmail.com", 
+				url: "/telegram",
 				method: "POST",
-				data: formMail.serialize() + "&coupon= " + promCodeForm + "&Итого= " + cartTotalForm,
-				dataType: "json"
+				data: allFields + '&some_param=' + cartTotalForm + '&some_code=' + promCodeForm + '&order_list=' + JSON.stringify(orderListArray),
+				success: function (data){
+					console.log(data);
+				},
 			}).done(function(){
 				simpleCart.empty();
 
@@ -170,20 +195,16 @@
 				$("#checkout").html('<div class="text-center">\
 					<h1>Спасибо за заказ</h1>\
 					<p><b>Отправка в 17:30</b><br>\
-					Ожидайте, мы с Вами свяжемся (через смс).<br>\
-					<span class="text-muted">Если в течение 2-х часов не пришла смс, то свяжитесь с нами сами: <a href="tel:+380675680230" title="Позвонить">+38 067 568 02 30</a> <br>Удобней всего писать в телеграмм: <a href="//t.me/snustop" target="_blank" title="Telegram"><i class="icon-telegram"></i>Написать в Telegram</a></p>\
+					Ожидайте, мы с Вами свяжемся (через смс или в месседжерах).<br>\
+					<span class="text-muted">Если в течение 2-х часов не пришла смс (в рабочее время), то <a href="#contactModal" data-toggle="modal" data-target="#contactModal">свяжитесь с нами сами</a>. <br>Удобней всего писать в телеграмм.<br>Подпишитесь на наш канал в телеграмме, чтоб не пропустить акции и новинки, обещаем не спамить.<br><a style="color: #fff;" class="btn btn-telegram mt-2" href="//t.me/snustopUA" target="_blank" rel="nofollow" title="Канал telegram">Подписаться<i class="icon-telegram"></i></a></p>\
 					<hr />\
-					<div class="text-left">\
+					<div class="text-left mb-4">\
 					<h2>Частые вопросы</h2>\
-					<p><b>Когда будет отправлен мой заказ?</b><br>Отправка каждый день в 17:30.</p>\
-					<p><b>Сколько стоит доставка?</b><br>Примерно 40 грн, +25 (если есть наложка).</p>\
-					<p><b>Когда придет мой заказ?</b><br>Завтра (иногда послезавтра, если это западная часть Украины). Вам придет смс-уведомление от Новой Почты.</p>\
-					<p><b>Скиньте номер накладной!</b><br>После отправки (в 17:30) в течение часа Вам придет номер накладной в смс, если не пришла, свяжитесь с нами!</p>\
-					<p><b>Остались вопросы?</b><br><a href="sms:+380-67-568-0230" title="Отправить смс">Пишите смс 0675680230</a>, <a href="//t.me/snustop" target="_blank" title="Telegram"><i class="icon-telegram"></i>пишите в Telegram</a>, либо звоните <a href="tel:+380675680230" title="Позвонить">+38 067 568 02 30</a> нам удобнее телеграмм или смс :)</p>\
+					<div class="accordion" id="accordionExample"><div class="card"><div class="card-header" id="headingOne"><h3 class="mb-0"><button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">Когда будет отправлен мой заказ?</button></h3></div><div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample"><div class="card-body">Отправка каждый день в 17:30. (в выходные раньше: суббота в 16:00, воскресенье 15:00), если Вы заказали позже этого времени, то заказ будет отправлен завтра.</div></div></div><div class="card"><div class="card-header" id="headingTwo"><h3 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">Сколько стоит доставка? </button></h3></div><div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample"><div class="card-body">40 грн если без наложенного платежа, дополнительно +25 (если есть наложка).</div></div></div><div class="card"><div class="card-header" id="headingThree"><h3 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree"> Когда придет мой заказ?</button></h3></div><div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample"><div class="card-body">Завтра (если это: Киев, Одесса, Львов, Днепр, Харьков и другие города), (иногда послезавтра, зависит от города). Вам придет смс-уведомление от Новой Почты.<br>Для более подробной информации <a href="#contactModal" data-toggle="modal" data-target="#contactModal">свяжитесь с нами</a>.</div></div></div><div class="card"><div class="card-header" id="headingFourth"><h3 class="mb-0"><button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFourth" aria-expanded="false" aria-controls="collapseFourth"> Остались вопросы?</button></h3></div><div id="collapseFourth" class="collapse" aria-labelledby="headingFourth" data-parent="#accordionExample"><div class="card-body"><a href="#contactModal" data-toggle="modal" data-target="#contactModal">Свяжитесь с нами</a> удобнее всего писать в телеграмме.</div></div></div></div>\
 					</div>\
 					<p><a class="btn btn-primary" href="/">Перейти на главную</a></span></p></div>');
 			}).fail(function(){
-				alert("Ошибка, Ваш браузер Safari, оформите заказ c другого браузера или свяжитесь с нами: +380675680230. \nЛибо напишите нам смс с заказом и куда отправить");
+				alert("Ошибка, свяжитесь с нами: +380675680230 (удобнее всего в телеграмме). \nЛибо напишите нам смс с заказом и куда отправить");
 			});
 		});
 		// Форма заказа, приходит на почту END
@@ -198,8 +219,6 @@
 			$('#phone').val(phoneStorage);
 			$('#message').val(messageStorage);
 			$('#promo-code').val(promoCodeStorage);
-
-			$('#discount-btn').click();
 		}	
 		// ЗАПОМИНАЕТ ДАННЫЕ телефон, сообщение и промо-код END
 
@@ -227,5 +246,9 @@
 
       document.addEventListener('copy', addLink);
 		// добавляет ссылку когда копируют END
+
+		// pjax start
+		$(document).pjax('a', '#pjax-container', {fragment: '#pjax-container'});
+		// pjax end
 
 	});
